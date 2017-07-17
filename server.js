dd4acf0d704fae803dae3613c90d1d388a1e72c3
Andrey Petrenko <andruka@gmail.com>
2017-07-17 17:45:00 +0300

    created a first file

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by pankist on 17/07/2017.
+ */
+var http = require("http");
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("It's alive!");
+    response.end();
+}).listen(3000);
