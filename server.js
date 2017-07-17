3971b8e06cdfc66d6d94869371f201519b5b9e8f
Andrey Petrenko <andruka@gmail.com>
2017-07-17 20:49:01 +0300

    buildpack changed to nodejs
    heroku buildpacks:set heroku/nodejs --app=random-picture-generator

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("It's alive!");
+    response.write("Hello, World!");
     response.end();
 }).listen(3000);
