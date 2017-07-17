ce923aa76d965bc8d80e955dee19666846db5fe7
Andrey Petrenko <andruka@gmail.com>
2017-07-18 01:25:59 +0300

    image 3

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+var http = require("http");
 var randomImageGenerator = require('random-image-generator'),
     fs = require('fs');
+
+
+http.createServer(function(request, response) {
+    randomImageGenerator(800, 600, function(err, image) {
+        fs.writeFile('random-image.jpg', image, function(err) {
+            // done, hopefully
+        });
+    });
+}).listen(process.env.PORT || 3000);
+
+
 
 /**
  * Takes a width and height parameter
  * Returns a Buffer containing a JPEG image
  */
-randomImageGenerator(800, 600, function(err, image) {
-    fs.writeFile('random-image.jpg', image, function(err) {
-        // done, hopefully
-    });
-});
