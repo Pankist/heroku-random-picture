bf2f7a039417634a72c5eea9c7e22a89e4000272
Andrey Petrenko <andruka@gmail.com>
2017-07-18 10:40:58 +0300

    testing Mykola

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello, World!");
+    response.write("Hello, Mykola!");
     response.end();
 }).listen(process.env.PORT || 3000);
